d2ae2e3206cb86cae6bed14259ff89948834ab03
rajesh <nrajesh593@gmail.com>
2022-05-23 22:10:13 +0530

    first commit

---
+++ products/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import path
+from .views import Product_view
+
+urlpatterns = [
+    
+    path('',Product_view.as_view()),
+    path('<int:id>', Product_view.as_view()),
+    # path('delete/<int:id>', Product_view.delete())
+]
